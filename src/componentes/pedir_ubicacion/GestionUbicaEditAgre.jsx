4d5d3866418bab08485c123ce0793c975b685a2f
Cajocoza96 <142413770+Cajocoza96@users.noreply.github.com>
2025-07-22 09:59:50 -1200

    correcion 4 height

--- src/componentes/pedir_ubicacion/GestionUbicaEditAgre.jsx
+++ src/componentes/pedir_ubicacion/GestionUbicaEditAgre.jsx
@@ -32,103 +32,107 @@ export default function GestionUbicaEditAgre() {
     const { isOnline } = useConexionInternet();
 
     return (
-        <div className="bg-blue-950 dark:bg-gray-800 mx-auto xss:h-screen-[100svh] h-screen 
+        <div className="bg-blue-950 dark:bg-gray-800 mx-auto xss:h-[100svh] h-screen">
+
+            <div className="w-full mx-auto xss:h-[100svh] h-screen 
                                 flex flex-col items-center relative justify-between">
 
-            <div className="mt-2 w-full mx-auto flex flex-col items-center justify-center gap-2">
+                <div className="mt-2 w-full mx-auto flex flex-col items-center justify-center gap-2">
 
-                <ValorValorValor
-                    
-                    valor1={
-                        <Link to="/">
-                            <HiHome className="text-white" />
-                        </Link>
-                    }
+                    <ValorValorValor
 
-                    valor2={<p className="text-white">Manage location</p>}
-                />
+                        valor1={
+                            <Link to="/">
+                                <HiHome className="text-white" />
+                            </Link>
+                        }
 
-                <div className={`bg-blue-700 dark:bg-gray-700
+                        valor2={<p className="text-white">Manage location</p>}
+                    />
+
+                    <div className={`bg-blue-700 dark:bg-gray-700
                                             w-full py-2 overflow-hidden
                                             flex items-center ${!isOnline ? "justify-center" : ""}`}>
 
-                    {isOnline ? (
-                        <AdvDeteccionAutoClima />
-                    ) : (
-                        <AdvDeteccionAutoClima mensajeNoConexion={mensajeNoConexion} /> 
-                    )}
-                    
-                </div>
+                        {isOnline ? (
+                            <AdvDeteccionAutoClima />
+                        ) : (
+                            <AdvDeteccionAutoClima mensajeNoConexion={mensajeNoConexion} />
+                        )}
 
-                <div className="w-[95%] h-100 xs:h-120 xs2:h-160 xs3:h-170 
+                    </div>
+
+                    <div className="w-[95%] h-100 xs:h-120 xs2:h-160 xs3:h-170 
                                 2xs:h-43 md:h-50 lg:h-100 2xl:h-190 
                                 overflow-y-auto overflow-x-hidden rounded-md">
 
-                    <div className="mx-auto w-full 
+                        <div className="mx-auto w-full 
                                             flex flex-col items-center">
-                        <ClimasUbicados
-                            iconoEliminar={modoEdicion ? <HiTrash /> : null}
-                            modoEdicion={modoEdicion}
-                        />
+                            <ClimasUbicados
+                                iconoEliminar={modoEdicion ? <HiTrash /> : null}
+                                modoEdicion={modoEdicion}
+                            />
+                        </div>
+
                     </div>
 
                 </div>
 
-            </div>
-
-            <div className="mt-1 mb-3 lg:mb-4 w-[90%]
+                <div className="mt-1 mb-3 lg:mb-4 w-[90%]
                 grid gap-4 overflow-hidden"
-                style={{
-                    gridTemplateColumns: modoEdicion || !mostrarBotonEditar ? '1fr' : '1fr 1fr'
-                }}>
+                    style={{
+                        gridTemplateColumns: modoEdicion || !mostrarBotonEditar ? '1fr' : '1fr 1fr'
+                    }}>
 
-                {!modoEdicion && (
-                    <>
-                        {mostrarBotonEditar && (
-                            <MotionBoton
-                                whileTap={{ scale: 0.95 }}
-                                transition={{ type: "spring", stiffness: 400, damping: 17 }}
-                                nombre="Edit"
-                                onClick={manejarEditar}
-                                className="p-1 w-full
+                    {!modoEdicion && (
+                        <>
+                            {mostrarBotonEditar && (
+                                <MotionBoton
+                                    whileTap={{ scale: 0.95 }}
+                                    transition={{ type: "spring", stiffness: 400, damping: 17 }}
+                                    nombre="Edit"
+                                    onClick={manejarEditar}
+                                    className="p-1 w-full
                                 border border-gray-300  dark:border-gray-800
                                 bg-gray-300 dark:bg-gray-950
                                 active:bg-gray-100 dark:active:bg-gray-600
                                 text-blue-700 dark:text-white
                                 rounded-md cursor-pointer"
-                            />
-                        )}
+                                />
+                            )}
 
-                        <Link to="/add-city-weather">
-                            <MotionBoton
-                                whileTap={{ scale: 0.95 }}
-                                transition={{ type: "spring", stiffness: 400, damping: 17 }}
-                                nombre="Add location"
-                                className="p-1 w-full
+                            <Link to="/add-city-weather">
+                                <MotionBoton
+                                    whileTap={{ scale: 0.95 }}
+                                    transition={{ type: "spring", stiffness: 400, damping: 17 }}
+                                    nombre="Add location"
+                                    className="p-1 w-full
                                 border border-gray-300  dark:border-gray-800
                                 bg-gray-300 dark:bg-gray-950
                                 active:bg-gray-100 dark:active:bg-gray-600
                                 text-blue-700 dark:text-white
                                 rounded-md cursor-pointer"
-                            />
-                        </Link>
-                    </>
-                )}
+                                />
+                            </Link>
+                        </>
+                    )}
 
-                {modoEdicion && (
-                    <MotionBoton
-                        whileTap={{ scale: 0.95 }}
-                        transition={{ type: "spring", stiffness: 400, damping: 17 }}
-                        nombre="Made"
-                        onClick={manejarHecho}
-                        className="p-1 w-full
+                    {modoEdicion && (
+                        <MotionBoton
+                            whileTap={{ scale: 0.95 }}
+                            transition={{ type: "spring", stiffness: 400, damping: 17 }}
+                            nombre="Made"
+                            onClick={manejarHecho}
+                            className="p-1 w-full
                             border border-gray-300  dark:border-gray-800
                             bg-gray-300 dark:bg-gray-950
                             active:bg-gray-100 dark:active:bg-gray-600
                             text-blue-700 dark:text-white
                             rounded-md cursor-pointer"
-                    />
-                )}
+                        />
+                    )}
+                </div>
+
             </div>
 
         </div>
