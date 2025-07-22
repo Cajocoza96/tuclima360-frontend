5a79eeb48d546a60071f7182dc6a449a6b5fd5e9
Cajocoza96 <142413770+Cajocoza96@users.noreply.github.com>
2025-07-22 09:31:14 -1200

    correccion scroll 2

--- src/componentes/scroll_control/ScrollToTop.jsx
+++ src/componentes/scroll_control/ScrollToTop.jsx
@@ -5,7 +5,7 @@ export default function ScrollToTop() {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    window.scrollTo({ top: 0, behavior: "instant" }); // Puedes usar 'smooth' si prefieres
+    window.scrollTo(0, 0);
   }, [pathname]);
 
   return null;
